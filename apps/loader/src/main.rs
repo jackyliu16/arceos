c2dd12c18fc75bd5995a191d08351a0ab1208e19
jackyliu16 <18922251299@163.com>
2023-11-16 19:48:25 +0800

    exec(2.1): finish
      thanks the help by 邝劲强
      mainly knownledge gaps: underlying representation of numbers
    
      Goal:
        insert the binrary size of the image before the image
    
      u16 -> 16bits = 4 hexadecimal digits = 2B
      dd's seek could make sure will not cover the previous one
      which
      [ 32M PFLASH ][ 2B Gap ] [ 2B U16 ]
      [ 32M PFLASH ][ 4B Gap            ] Image

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -1,6 +1,7 @@
 
 #![cfg_attr(feature = "axstd", no_std)]
 #![cfg_attr(feature = "axstd", no_main)]
+
 use core::mem::size_of;
 
 #[cfg(feature = "axstd")]
@@ -9,11 +10,19 @@ const PLASH_START: usize = 0x22000000;
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
     let apps_start = PLASH_START as *const u8;
-    let apps_size: usize = 32; // Dangerous!!! We need to get accurate size of apps
+    let byte = unsafe {
+      core::slice::from_raw_parts(apps_start, size_of::<u32>())
+    };
+    let apps_size = u32::from_be_bytes([byte[0], byte[1], byte[2], byte[3]]);
+
     println!("Load payload ...");
 
-    let code = unsafe { 
-      core::slice::from_raw_parts(apps_start, apps_size) 
+    println!("sizeByte: {byte:?}");
+    println!("size: {apps_size}");
+
+    let code: &[u8] = unsafe { 
+      // core::slice::from_raw_parts(apps_start.offset(size_of::<u16>() as isize), apps_size as usize) 
+      core::slice::from_raw_parts(apps_start.offset(size_of::<u32>() as isize), apps_size as usize) 
     };
 
     println!("content: {:?}: ", code);
--- playload/generater.sh
+++ playload/generater.sh
@@ -13,8 +13,14 @@ cd "$BASE_DIR/hello_app"
 cargo build --target riscv64gc-unknown-none-elf --release
 # remove symbol information
 rust-objcopy --binary-architecture=riscv64 --strip-all -O binary ../../target/riscv64gc-unknown-none-elf/release/hello_app ./hello_app.bin
+
+APP_BIN_SIZE=$(stat -c%s "./hello_app.bin")
+echo $APP_BIN_SIZE
+printf "%04x" $APP_BIN_SIZE | xxd -r -p > ./size.bin
+
 # Using Zero to fill the block to 32M(32Times, one times for 1M)
 dd if=/dev/zero of=./apps.bin bs=1M count=32
 # Add origin app into the end of the file (not cover)
-dd if=./hello_app.bin of=./apps.bin conv=notrunc
+dd if=./size.bin of=./apps.bin conv=notrunc       bs=1B seek=2
+dd if=./hello_app.bin of=./apps.bin conv=notrunc  bs=1B seek=4
 mv $BASE_DIR/hello_app/apps.bin "$BASE_DIR/apps.bin"
