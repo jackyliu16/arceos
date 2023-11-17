b208299f6e95d42ec0b188dd6e09d664bac07c55
jackyliu16 <18922251299@163.com>
2023-11-17 10:12:05 +0800

    base(2.4): finish

--- playload/hello_app/src/main.rs
+++ playload/hello_app/src/main.rs
@@ -1,14 +1,26 @@
+#![feature(asm_const)]
 #![no_std]
 #![no_main]
 
 use core::panic::PanicInfo;
+const SYS_HELLO: usize = 1;
+const SYS_PUTCHAR: usize = 2;
+const SYS_TERMINATE: usize = 3;
 
 #[no_mangle]
 unsafe extern "C" fn _start() -> ! {
- core::arch::asm!(
-  "wfi",
-  options(noreturn)
- )
+    let arg0: u8 = b'C';
+    core::arch::asm!("
+        li      t0, {abi_num}
+        slli    t0, t0, 3
+        add     t1, a7, t0
+        ld      t1, (t1)
+        jalr    t1
+        wfi",
+        abi_num = const SYS_PUTCHAR,
+        in("a0") arg0,
+        options(noreturn),
+    )
 }
 
 #[panic_handler]
