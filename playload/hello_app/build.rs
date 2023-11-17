d61374d05736db147ec7ddd33c35d0362a17591a
jackyliu16 <18922251299@163.com>
2023-11-17 20:26:50 +0800

    feat(2.4): update linker script to enable &str

--- playload/hello_app/build.rs
+++ playload/hello_app/build.rs
@@ -1,15 +1,5 @@
 //! A simple linker script
 
 fn main() {
-  let arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
-  if !arch.contains("riscv") {
-    println!("arch {} is not allow in this script", arch);
-  }
-
-  let ld_content = std::fs::read_to_string("linker.ld").unwrap();
-  let ld_content = ld_content.replace("%APP_SIZE%", "32");
-
-  std::fs::write("linker_with_app_size.ld", ld_content).unwrap();
-
-  println!("cargo:rustc-link-arg=-T./playload/hello_app/linker_with_app_size.ld");
+    println!("cargo:rustc-link-arg=-T./playload/hello_app/linker.ld");
 }
